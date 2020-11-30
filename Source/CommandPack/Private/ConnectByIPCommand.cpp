b86e4acf33f5f73024443f7bb8fcb99c97b38f7c
Stepan <trofimovjava@gmail.com>
2020-11-30 21:57:33 +0300

    Add FConnectByIPCommandData

--- Source/CommandPack/Private/ConnectByIPCommand.cpp
+++ Source/CommandPack/Private/ConnectByIPCommand.cpp
@@ -2,25 +2,22 @@
 
 
 #include "ConnectByIPCommand.h"
-
 #include "Kismet/KismetSystemLibrary.h"
 
 bool UConnectByIPCommand::Init_Implementation(
 	UObject* InWorldContextObject,
-	const FString& InIP,
-	APlayerController* InPlayerController
+	FConnectByIPCommandData InCommandData
 )
 {
 	const auto bResult = InitWorldContext(InWorldContextObject);
 
-	IP = InIP;
-	PlayerController = InPlayerController;
+	CommandData = InCommandData;
 
 	return bResult;
 }
 
 void UConnectByIPCommand::Execute_Implementation()
 {
-	const auto Command = FString(TEXT("open ")).Append(IP);
-	UKismetSystemLibrary::ExecuteConsoleCommand(WorldContextObject, Command, PlayerController);
+	const auto Command = FString(TEXT("open ")).Append(CommandData.IP);
+	UKismetSystemLibrary::ExecuteConsoleCommand(WorldContextObject, Command, CommandData.PlayerController);
 }
